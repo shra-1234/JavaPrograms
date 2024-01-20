d0d61572c5d8741a8419e61532cc09d78c81cb4f
Shraddha B Math <117364109+shra-1234@users.noreply.github.com>
2024-01-20 20:47:36 +0530

    Adds two string using add operator.

---
+++ concateString1.java
@@ -0,0 +1,11 @@
+/**
+ * concateString
+ */
+public class concateString1 {
+
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World!";
+        System.out.println(str1+" "+str2);//using '+' operator
+    }
+}
