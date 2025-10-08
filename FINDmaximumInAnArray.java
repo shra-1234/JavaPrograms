cc8aefac83ec0141cf2ac07f4598eda05675925c
Shraddha B Math <117364109+shra-1234@users.noreply.github.com>
2025-10-08 16:53:17 +0530

    Update FINDmaximumInAnArray.java
    Uncommented the commits

--- FINDmaximumInAnArray.java
+++ FINDmaximumInAnArray.java
@@ -9,7 +9,7 @@ class Codechef
 		// your code goes here
         Scanner sc=new Scanner(System.in);
         int T=sc.nextInt();
-        //while(T>0){
+        while(T>0){
             int N=sc.nextInt();
             int arr[]=new int[N];
             for(int i=0;i<N;i++){
@@ -22,7 +22,7 @@ class Codechef
                 }
             }
             System.out.println(max);
-            //T--;
-        //}
+            T--;
+        }
 	}
 }
